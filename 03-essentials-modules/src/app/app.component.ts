c09aeb43177810378e499c681758feb0a1a8acd5
Shaban-Eissa <shabaneissa767@gmail.com>
2024-08-28 09:45:59 +0300

    refactor: Using app module instead of standalone

--- 03-essentials-modules/src/app/app.component.ts
+++ 03-essentials-modules/src/app/app.component.ts
@@ -1,13 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { DUMMY_USERS } from './dummy-users';
-import { HeaderComponent } from './header/header.component';
-import { TasksComponent } from './tasks/tasks.component';
-import { UserComponent } from './user/user.component';
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
