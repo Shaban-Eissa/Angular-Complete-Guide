ae5c83bfce68021fcd992e2356b5792ef7a3e288
Shaban-Eissa <shabaneissa767@gmail.com>
2024-08-29 15:33:49 +0300

    refactor: Update new-ticket component to include form submission functionality

--- 06-components-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 06-components-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,13 +1,17 @@
-import { Component, input } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent, ControlComponent],
+  imports: [FormsModule, ButtonComponent, ControlComponent],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
 })
 export class NewTicketComponent {
+  onSubmit() {
+    console.log('LOG MESSAGE');
+  }
 }
