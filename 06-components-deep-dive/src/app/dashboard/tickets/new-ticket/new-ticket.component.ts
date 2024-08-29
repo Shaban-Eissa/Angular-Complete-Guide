8b73cc1bde66fc73db39134f7170ff561e758d85
Shaban-Eissa <shabaneissa767@gmail.com>
2024-08-29 12:05:46 +0300

    refactor: Update new-ticket component to use reusable control component (Custom Element)

--- 06-components-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 06-components-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
+import { ControlComponent } from '../../../shared/control/control.component';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, ControlComponent],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
 })
-export class NewTicketComponent {}
+export class NewTicketComponent {
+}
