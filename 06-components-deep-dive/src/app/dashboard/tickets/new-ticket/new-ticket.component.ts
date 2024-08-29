a3e43dfd12c14c7b14212f9a2f143fc55c5b3e23
Shaban-Eissa <shabaneissa767@gmail.com>
2024-08-29 15:52:56 +0300

    refactor: Fix typo in new-ticket component ViewChild import

--- 06-components-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 06-components-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, viewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
@@ -11,11 +11,12 @@ import { ControlComponent } from '../../../shared/control/control.component';
   styleUrl: './new-ticket.component.css',
 })
 export class NewTicketComponent {
-  @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+  // @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+  private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
   onSubmit(titleInput: string, textInput: string) {
     console.log('LOG MESSAGE');
     console.log(titleInput, textInput);
 
-    this.form?.nativeElement.reset();
+    this.form()?.nativeElement.reset();
   }
 }
