4091c52b6c11939dddc8772e9f93459f72e55315
Shaban-Eissa <shabaneissa767@gmail.com>
2024-08-31 19:17:04 +0300

    feat: Add host directives

--- 07-directives-deep-dive/src/app/learning-resources/learning-resources.component.ts
+++ 07-directives-deep-dive/src/app/learning-resources/learning-resources.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { LogDirective } from '../log.directive';
 import { SafeLinkDirective } from '../safe-link.directive';
 
 @Component({
   selector: 'app-learning-resources',
-  imports: [SafeLinkDirective],
+  imports: [SafeLinkDirective, LogDirective],
   templateUrl: './learning-resources.component.html',
   styleUrl: './learning-resources.component.css',
   standalone: true,
+  hostDirectives: [LogDirective],
 })
 export class LearningResourcesComponent {}
