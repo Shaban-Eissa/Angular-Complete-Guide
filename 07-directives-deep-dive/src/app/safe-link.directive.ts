c5516bc75d69f73a9c0fbac802bfd276cd02ca39
Shaban-Eissa <shabaneissa767@gmail.com>
2024-08-31 18:36:13 +0300

    feat: Add appSafeLink directive to learning resources component with host listening

--- 07-directives-deep-dive/src/app/safe-link.directive.ts
+++ 07-directives-deep-dive/src/app/safe-link.directive.ts
@@ -3,9 +3,19 @@ import { Directive } from '@angular/core';
 @Directive({
   selector: 'a[appSafeLink]',
   standalone: true,
+  host: {
+    '(click)': 'onConfirmPageLeave($event)',
+  },
 })
 export class SafeLinkDirective {
   constructor() {
     console.log('SafeLinkDirective instantiated');
+  }
+
+  onConfirmPageLeave(event: MouseEvent) {
+    const confirmed = window.confirm('Are you sure you want to leave?');
+    if (!confirmed) {
+      event.preventDefault();
+    }
   }
 }
