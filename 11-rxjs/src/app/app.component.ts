0b61aaa7a2647f0752740293ef8e76d10bc80458
Shaban-Eissa <shabaneissa767@gmail.com>
2024-09-01 15:48:30 +0300

    🎉 Initial Commit: Adding files for module 11

---
+++ 11-rxjs/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+import { interval } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+})
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    interval(1000).subscribe({
+      next: (val) => console.log(val),
+    });
+  }
+}
