c3a6f42551b341bbe51ff514ab0c92ccf31a3b1a
Shaban-Eissa <shabaneissa767@gmail.com>
2024-09-01 15:27:05 +0300

    ✨ New Feature: Using BehaviorSubject to trigger change detection (ChangeDetectorRef)

--- 10-change-detection/src/app/messages/messages-list/messages-list.component.ts
+++ 10-change-detection/src/app/messages/messages-list/messages-list.component.ts
@@ -1,4 +1,10 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  DestroyRef,
+  inject,
+} from '@angular/core';
 import { MessagesService } from '../messages.service';
 
 @Component({
@@ -10,7 +16,24 @@ import { MessagesService } from '../messages.service';
 })
 export class MessagesListComponent {
   private messagesService = inject(MessagesService);
-  messages = this.messagesService.allMessages;
+
+  private cdRef = inject(ChangeDetectorRef);
+  private destroyRef = inject(DestroyRef);
+
+  messages: string[] = [];
+
+  ngOnInit() {
+    const subscription = this.messagesService.messages$.subscribe(
+      (messages) => {
+        this.messages = messages;
+        this.cdRef.markForCheck();
+      }
+    );
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
+
   get debugOutput() {
     console.log('[MessagesList] "debugOutput" binding re-evaluated.');
     return 'MessagesList Component Debug Output';
