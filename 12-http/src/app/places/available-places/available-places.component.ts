41d987b98c9b81fff1d8ed5ef8cbb39097cef50c
Shaban-Eissa <shabaneissa767@gmail.com>
2024-09-01 20:26:07 +0300

    👌 IMPROVE: Handling request errors

--- 12-http/src/app/places/available-places/available-places.component.ts
+++ 12-http/src/app/places/available-places/available-places.component.ts
@@ -1,7 +1,7 @@
 import { Component, DestroyRef, inject, signal } from '@angular/core';
 
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs';
+import { catchError, map, throwError } from 'rxjs';
 import { Place } from '../place.model';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { PlacesComponent } from '../places.component';
@@ -18,6 +18,7 @@ export class AvailablePlacesComponent {
   private httpClient = inject(HttpClient);
 
   isFetching = signal(false);
+  error = signal('');
   private destroyRef = inject(DestroyRef);
 
   private HTTPClient = inject(HttpClient);
@@ -26,10 +27,25 @@ export class AvailablePlacesComponent {
     this.isFetching.set(true);
     const subscription = this.httpClient
       .get<{ places: Place[] }>('http://localhost:3000/places')
-      .pipe(map((resData) => resData.places))
+      .pipe(
+        map((resData) => resData.places),
+        catchError((error) => {
+          console.log(error);
+          return throwError(
+            () =>
+              new Error(
+                'Something went wrong fetching the available places. Please try again later.'
+              )
+          );
+        })
+      )
       .subscribe({
         next: (places) => {
           this.places.set(places);
+        },
+        error: (error: Error) => {
+          this.isFetching.set(false);
+          this.error.set(error.message);
         },
         complete: () => {
           this.isFetching.set(false);
