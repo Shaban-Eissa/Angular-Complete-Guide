cba2e563afec2d4b00775747eba5d5e5d8f3f9d5
Shaban-Eissa <shabaneissa767@gmail.com>
2024-09-02 16:48:55 +0300

    💥 NEW: Adding nested routes

--- 14-routing/src/app/app.routes.ts
+++ 14-routing/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 
+import { NewTaskComponent } from './tasks/new-task/new-task.component';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
+import { TasksComponent } from './tasks/tasks.component';
 import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
 
 export const routes: Routes = [
@@ -9,7 +11,17 @@ export const routes: Routes = [
     component: NoTaskComponent,
   },
   {
-    path: 'users/:userId',
+    path: 'users/:userId', // <your-domain>/users/<uid>
     component: UserTasksComponent,
+    children: [
+      {
+        path: 'tasks', // <your-domain>/users/<uid>/tasks
+        component: TasksComponent,
+      },
+      {
+        path: 'tasks/new',
+        component: NewTaskComponent,
+      },
+    ],
   },
 ];
