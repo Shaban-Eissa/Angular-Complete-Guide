dd7763e56d59a298baf8a060db3bd5edad8ebeea
Shaban-Eissa <shabaneissa767@gmail.com>
2024-09-03 09:43:38 +0300

    ✨ New Feature: Sorting tasks based on query params

--- 14-routing/src/app/app.routes.ts
+++ 14-routing/src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 
 import { NotFoundComponent } from './not-found/not-found.component';
-import { NewTaskComponent } from './tasks/new-task/new-task.component';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
-import { TasksComponent } from './tasks/tasks.component';
 import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
+
+import { routes as userRoutes } from './users/users.routes';
 
 export const routes: Routes = [
   {
@@ -14,21 +14,7 @@ export const routes: Routes = [
   {
     path: 'users/:userId', // <your-domain>/users/<uid>
     component: UserTasksComponent,
-    children: [
-      {
-        path: '',
-        redirectTo: 'tasks',
-        pathMatch: 'full',
-      },
-      {
-        path: 'tasks', // <your-domain>/users/<uid>/tasks
-        component: TasksComponent,
-      },
-      {
-        path: 'tasks/new',
-        component: NewTaskComponent,
-      },
-    ],
+    children: userRoutes,
   },
   {
     path: '**',
