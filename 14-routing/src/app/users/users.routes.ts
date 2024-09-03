eae0f94e47cea208a5834e7f9c284106dee3d4c2
Shaban-Eissa <shabaneissa767@gmail.com>
2024-09-03 10:11:23 +0300

    💥 NEW: Using runGuardsAndResolvers in users routes

--- 14-routing/src/app/users/users.routes.ts
+++ 14-routing/src/app/users/users.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 
 import { NewTaskComponent } from '../tasks/new-task/new-task.component';
-import { TasksComponent } from '../tasks/tasks.component';
+import { TasksComponent, resolveUserTasks } from '../tasks/tasks.component';
 
 export const routes: Routes = [
   {
@@ -12,6 +12,10 @@ export const routes: Routes = [
   {
     path: 'tasks', // <your-domain>/users/<uid>/tasks
     component: TasksComponent,
+    runGuardsAndResolvers: 'paramsOrQueryParamsChange',
+    resolve: {
+      userTasks: resolveUserTasks,
+    },
   },
   {
     path: 'tasks/new',
