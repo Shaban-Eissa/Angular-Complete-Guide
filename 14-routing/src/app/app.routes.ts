824370221cd94858e1f82b00e3fd67e2640a361c
Shaban-Eissa <shabaneissa767@gmail.com>
2024-09-03 10:21:32 +0300

    🔒 Security: Using route guards (canMatch)

--- 14-routing/src/app/app.routes.ts
+++ 14-routing/src/app/app.routes.ts
@@ -1,4 +1,4 @@
-import { Routes } from '@angular/router';
+import { CanMatchFn, RedirectCommand, Router, Routes } from '@angular/router';
 
 import { NotFoundComponent } from './not-found/not-found.component';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
@@ -7,7 +7,17 @@ import {
   UserTasksComponent,
 } from './users/user-tasks/user-tasks.component';
 
+import { inject } from '@angular/core';
 import { routes as userRoutes } from './users/users.routes';
+
+const dummyCanMatch: CanMatchFn = () => {
+  const router = inject(Router);
+  const shouldGetAccess = Math.random();
+  if (shouldGetAccess < 0.5) {
+    return true;
+  }
+  return new RedirectCommand(router.parseUrl('/unauthorized'));
+};
 
 export const routes: Routes = [
   {
@@ -18,6 +28,7 @@ export const routes: Routes = [
     path: 'users/:userId', // <your-domain>/users/<uid>
     component: UserTasksComponent,
     children: userRoutes,
+    canMatch: [dummyCanMatch],
     data: {
       message: 'Hello from app.routes',
     },
